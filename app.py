0ec3275c3da3a18ab4e8321ace17febf006b76cf
HP319193 <davidharryporter@gmail.com>
2024-04-19 16:53:02 +0300

    Update

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.docstore.document import Document
 
+from openai import OpenAI
 from dotenv import load_dotenv
-import os, random
+import os, random, json
 
 load_dotenv()
 
@@ -23,17 +24,53 @@ pinecone_index = os.getenv("INDEX")
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
 metadata_list = ['fullname', 'mediator email', 'mediator profile on mediate.com', 'mediator Biography']
+metadata_value = ['Name', "Email", "Profile", "Biography"]
 
 embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
+openai_client = OpenAI(api_key=openai_api_key)
 def search(message, history):
+    tools = [
+            {
+                "type": "function", 
+                "function": {
+                    "name": "mediator_search",
+                    "description": "Extract how many mediators user want to search.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "mediator": {
+                                "type": "number",
+                                "description": "The number of mediators that user want to search",
+                                "default": 1
+                            }
+                        },
+                        "required": ["mediator"]
+                    }
+                }
+            }
+        ]
+    
+    response = openai_client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            messages=[
+                    {"role": "system", "content": "Please extract how many mediators users want to search."},
+                    {"role": "user", "content": message}
+                ],
+                tools=tools,
+    )
+
+    number_str = response.choices[0].message.tool_calls[0].function.arguments
+    
+    mediator_num = json.loads(number_str)['mediator']
+
     template = """"""
-    prompt = "You are a professional mediator search expert. Your role is to generate answer to question depending on the context. Your answer must be from context. You answer must include mediator's email, mediator profile on mediate.com, mediator Biography and the reason why this mediator is appropriate to human input. You have to mention that current search result is not completed and chatbot can search different mediators or mediator."
+    prompt = "You are a professional mediator information analyzer. You have to write why the following context is related to human's message. Please write 3 or 4 sentences."
     
     end = """Context: {context}
         Chat history: {chat_history}
         Human: {human_input}
         Your Response as Chatbot:"""
+    
     template += prompt + end
 
     prompt = PromptTemplate(
@@ -42,27 +79,52 @@ def search(message, history):
         )
 
     memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
-    # docsearch = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-    docsearch = PineconeVectorStore.from_existing_index(index_name = pinecone_index, embedding = embeddings)
 
     print(message)
     start_time = time.time()
-    docs = docsearch.similarity_search(message, k=15)
+
+    pc = Pinecone(api_key=pinecone_api_key)
+
+    embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+    
+    index = pc.Index(pinecone_index)
+
+    results = index.query(
+        vector=embeddings.embed_query(message),
+        top_k=1,
+        include_metadata=True
+    )
     end_time = time.time()
 
     print("Search Time =>", end_time-start_time)
     
     new_docs = []
-    for doc in docs:
-        page_content = ""
-        for metadata in metadata_list:
-            page_content += f"{metadata}: {doc.metadata[metadata]}" + "\n"
+    new_data = []
+    for result in results['matches']:
+        if result['score'] > 0.75:
+            data = {}
+            for metadata in metadata_list:                
+                data[metadata] = result['metadata'][metadata]
 
-        new_doc =  Document(page_content=page_content)
+            new_data.append(data)
+    
+    random.shuffle(new_data)
 
-        new_docs.append(new_doc)
+    answer = ""
+    for index, new_datum in enumerate(new_data):
+        if index < mediator_num:
+            answer = f"{index+1}\n"
+            content = ""
+            for metadata_index, metadata in enumerate(metadata_list):
+                content += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
+                answer += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
 
-    random.shuffle(new_docs)
+            answer += "\n\n"
+            new_doc = Document(page_content=answer)
+            new_docs.append(new_doc)
+        else:
+            break
+
     chat_openai = ChatOpenAI(model='gpt-4-1106-preview', 
             openai_api_key=openai_api_key)
 
@@ -73,8 +135,9 @@ def search(message, history):
     end_time = time.time()
 
     print("Query Time =>", end_time-start_time)
-    print(output)
-    return output["output_text"]
+    
+    answer += f"Why appropriate: {output['output_text']}"
+    return answer
 
 chatbot = gr.Chatbot(avatar_images=["user.png", "bot.jpg"], height=600)
 
