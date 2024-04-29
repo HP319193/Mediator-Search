0eb45c2def1d0029a850cbcd38c9eef5582c2bfd
HP319193 <davidharryporter@gmail.com>
2024-04-29 21:45:41 +0300

    General question processing

--- app.py
+++ app.py
@@ -74,145 +74,155 @@ def getMetadata(message):
     response = openai_client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=[
-                {"role": "system", "content": f"You are a helpful astronomic assistant. Your role is to extract information about mediator from user's message."},
+                {"role": "system", "content": f"You are a professional mediation field searcher. Your role is to extract information about mediator from user's message."},
                 {"role": "user", "content": message}
             ],
         tools=tools
     )
 
     print("Message =>", response.choices[0])
-    return response.choices[0].message.tool_calls[0].function.arguments
+
+    try:
+        data = response.choices[0].message.tool_calls[0].function.arguments
+        search_status = True
+    
+    except:
+        search_status = False
+        data = response.choices[0].message.content
+
+    return {"search_status": search_status, "data": data}
 
 def search(message, history):
-    metadata = json.loads(getMetadata(message=message))
-    print(metadata)
-    try:
-        practice_data = metadata['mediator areas of practice']
-        del metadata['mediator areas of practice']
-    except:
-        practice_data = ""
-
-    
-    # if "mediator areas of practice" in metadata:
-    #     metadata['mediator areas of practice'] = {"$in":[metadata['mediator areas of practice']]}
-    
-    print("metadata =>", metadata)
-    tools = [
-            {
-                "type": "function", 
-                "function": {
-                    "name": "mediator_search",
-                    "description": "Extract how many mediators user want to search.",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "mediator": {
-                                "type": "number",
-                                "description": "The number of mediators that user want to search. If user ask a list of mediators, it means user want to search 3 mediators. If user's message don't have information about the number of mediators, you have to respond with 1.",
-                                "default": 1
-                            }
-                        },
-                        "required": ["mediator"]
+    data = getMetadata(message=message)
+    search_status = data['search_status']
+    answer = ""
+    if search_status == True:
+        metadata = json.loads(data['data'])
+        print(metadata)
+        try:
+            practice_data = metadata['mediator areas of practice']
+            del metadata['mediator areas of practice']
+        except:
+            practice_data = ""
+        
+        print("metadata =>", metadata)
+        tools = [
+                {
+                    "type": "function", 
+                    "function": {
+                        "name": "mediator_search",
+                        "description": "Extract how many mediators user want to search.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "mediator": {
+                                    "type": "number",
+                                    "description": "The number of mediators that user want to search. If user ask a list of mediators, it means user want to search 3 mediators. If user's message don't have information about the number of mediators, you have to respond with 1.",
+                                    "default": 1
+                                }
+                            },
+                            "required": ["mediator"]
+                        }
                     }
                 }
-            }
-        ]
-    
-    response = openai_client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                    {"role": "system", "content": "Please extract how many mediators users want to search."},
-                    {"role": "user", "content": message}
-                ],
-                tools=tools,
-    )
-
-    number_str = response.choices[0].message.tool_calls[0].function.arguments
-    
-    mediator_num = json.loads(number_str)['mediator']
-
-    print(mediator_num)
-
-    template = """"""
-    prompt = "You are a professional mediator information analyzer. You have to write the reason why following mediators are appropriated to human's message. You shouldn't write mediator's information again. You have to analyze the mediators at once. You have to only write the reason within 2 sentences. "
-    
-    end = """Context: {context}
-        Chat history: {chat_history}
-        Human: {human_input}
-        Your Response as Chatbot:"""
-    
-    template += prompt + end
-
-    prompt = PromptTemplate(
-        input_variables=["chat_history", "human_input", "context"], 
-        template=template
+            ]
+        
+        response = openai_client.chat.completions.create(
+                model="gpt-4-1106-preview",
+                messages=[
+                        {"role": "system", "content": "Please extract how many mediators users want to search."},
+                        {"role": "user", "content": message}
+                    ],
+                    tools=tools,
         )
 
-    memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
-
-    # print(message)
-    start_time = time.time()
-
-    pc = Pinecone(api_key=pinecone_api_key)
-
-    embeddings = OpenAIEmbeddings(api_key=openai_api_key)
-    
-    index = pc.Index(pinecone_index)
-
-    results = index.query(
-        vector=embeddings.embed_query(message),
-        top_k=748,
-        filter=metadata,
-        include_metadata=True
-    )
-
-    print("num of result =>", len(results['matches']))
-    end_time = time.time()
-
-    print("Search Time =>", end_time-start_time)
-    
-    new_docs = []
-    new_data = []
-    for result in results['matches']:
-        print(result['score'])
-        data = {}
-        for metadata in metadata_list:      
-            data[metadata] = result['metadata'][metadata]
+        number_str = response.choices[0].message.tool_calls[0].function.arguments
         
-        if practice_data in result['metadata']['mediator areas of practice']:
-            new_data.append(data)
+        mediator_num = json.loads(number_str)['mediator']
 
-    print(len(new_data))
-    random.shuffle(new_data)
+        print(mediator_num)
 
-    answer = ""
-    for index, new_datum in enumerate(new_data):
-        if index < mediator_num:
-            answer += f"{index+1}\n"
-            content = ""
-            for metadata_index, metadata in enumerate(metadata_list):
-                content += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
-                answer += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
+        template = """"""
+        prompt = "You are a professional mediator information analyzer. You have to write the reason why following mediators are appropriated to human's message. You shouldn't write mediator's information again. You have to analyze the mediators at once. You have to only write the reason within 2 sentences. "
+        
+        end = """Context: {context}
+            Chat history: {chat_history}
+            Human: {human_input}
+            Your Response as Chatbot:"""
+        
+        template += prompt + end
 
-            answer += "\n\n"
-            new_doc = Document(page_content=answer)
-            new_docs.append(new_doc)
-        else:
-            break
+        prompt = PromptTemplate(
+            input_variables=["chat_history", "human_input", "context"], 
+            template=template
+            )
 
-    chat_openai = ChatOpenAI(model='gpt-4-1106-preview', 
-            openai_api_key=openai_api_key)
+        memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
 
-    print(new_docs)
-    chain = load_qa_chain(chat_openai, chain_type="stuff",  prompt=prompt, memory=memory)
-    start_time = time.time()
-    output = chain({"input_documents": new_docs, "human_input": message}, return_only_outputs=False)
-    end_time = time.time()
+        # print(message)
+        start_time = time.time()
 
-    print("Query Time =>", end_time-start_time)
-    
-    answer += f"Why appropriate: {output['output_text']}"
-    
+        pc = Pinecone(api_key=pinecone_api_key)
+
+        embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+        
+        index = pc.Index(pinecone_index)
+
+        results = index.query(
+            vector=embeddings.embed_query(message),
+            top_k=748,
+            filter=metadata,
+            include_metadata=True
+        )
+
+        print("num of result =>", len(results['matches']))
+        end_time = time.time()
+
+        print("Search Time =>", end_time-start_time)
+        
+        new_docs = []
+        new_data = []
+        for result in results['matches']:
+            data = {}
+            for metadata in metadata_list:      
+                data[metadata] = result['metadata'][metadata]
+            
+            if practice_data in result['metadata']['mediator areas of practice']:
+                new_data.append(data)
+
+        print(len(new_data))
+        random.shuffle(new_data)
+
+        
+        for index, new_datum in enumerate(new_data):
+            if index < mediator_num:
+                answer += f"{index+1}\n"
+                content = ""
+                for metadata_index, metadata in enumerate(metadata_list):
+                    content += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
+                    answer += f"{metadata_value[metadata_index]}: {new_datum[metadata]} \n"
+
+                answer += "\n\n"
+                new_doc = Document(page_content=answer)
+                new_docs.append(new_doc)
+            else:
+                break
+
+        chat_openai = ChatOpenAI(model='gpt-4-1106-preview', 
+                openai_api_key=openai_api_key)
+
+        print(new_docs)
+        chain = load_qa_chain(chat_openai, chain_type="stuff",  prompt=prompt, memory=memory)
+        start_time = time.time()
+        output = chain({"input_documents": new_docs, "human_input": message}, return_only_outputs=False)
+        end_time = time.time()
+
+        print("Query Time =>", end_time-start_time)
+        
+        answer += f"Why appropriate: {output['output_text']}"
+    else:
+        answer = data['data']
+
     return answer
 
 chatbot = gr.Chatbot(avatar_images=["user.png", "bot.jpg"], height=600)
